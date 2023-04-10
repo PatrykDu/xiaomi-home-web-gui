dc465c02366a49536890c227cc2603da6a24e2ee
dudkap <dudekzwp@gmail.com>
2023-04-10 03:01:20 +0200

    git init

---
+++ actions.py
@@ -0,0 +1,123 @@
+from miio.integrations.light.yeelight.yeelight import Yeelight
+from miio.miot_device import MiotDevice
+from dataclasses import dataclass
+from typing import List, Dict
+import json
+
+
+DEVICES_JSON_PATH = 'devices.json'
+
+
+@dataclass
+class Device:
+    name: str
+    ip: str
+    token: str
+    device_type: str
+
+
+class DeviceResource:
+
+    def create_device(device_item: Dict[str, str]) -> Device:
+        return Device(
+            name=device_item[0],
+            ip=device_item[1]["ip"],
+            token=device_item[1]["token"],
+            device_type=None
+        )
+
+    def create_list_of_devices(data, type_of_device) -> List[Device]:
+        devices_data = data[type_of_device]
+        devices_list = []
+        for device in devices_data.items():
+            device_object = DeviceResource.create_device(device)
+            devices_list.append(device_object)
+        for device in devices_list:
+            device.device_type = type_of_device
+        return devices_list
+
+    def change_json_to_devices(devices_json_path: str) -> List[Device]:
+        with open(devices_json_path, 'r') as file:
+            data = json.load(file)
+
+        all_devices_list = []
+        for type in data.keys():
+            device_list = DeviceResource.create_list_of_devices(data, type)
+            all_devices_list += device_list
+
+        return all_devices_list
+
+
+# Yeelight lights handling
+
+def create_yeelight_device(device: Device) -> (Yeelight):
+    return Yeelight(device.ip,
+                    device.token)
+
+
+def power_status_yeelight(device: Yeelight):
+    device_status = Yeelight.status(device)
+    if device_status.data['power'] == 'on':
+        return True
+    else:
+        return False
+
+
+def toggle_yeelight_device(device: Yeelight):
+    device.toggle()
+
+
+# Cuco lights handing
+
+def create_cuco_light_s14_group(cuco_devices_list: List[Device]) -> List[MiotDevice]:
+    cuco_group_list = []
+    for device in cuco_devices_list:
+        device_object = MiotDevice(device.ip, device.token)
+        cuco_group_list.append(device_object)
+    return cuco_group_list
+
+
+def power_status_cuco_light_s14_group(cuco_group_list: List[MiotDevice]):
+    """ Works only with Model: cuco.light.sl4 documentation about properties can be found here:
+    https://miot-spec.org/miot-spec-v2/instance?type=urn:miot-spec-v2:device:light:0000A001:cuco-sl4:1"""
+
+    status1 = cuco_group_list[0].get_property_by(2, 1)[0]['value']
+    status2 = cuco_group_list[1].get_property_by(2, 1)[0]['value']
+    if status1 and status2:
+        return True
+    else:
+        return False
+
+
+def on_cuco_light_s14(cuco_group_list: List[MiotDevice]) -> None:
+    for cuco in cuco_group_list:
+        cuco.set_property_by(2, 1, True)
+
+
+def off_cuco_light_s14(cuco_group_list: List[MiotDevice]) -> None:
+    for cuco in cuco_group_list:
+        cuco.set_property_by(2, 1, False)
+
+
+# Create list of all devices
+devices_list = DeviceResource.change_json_to_devices(DEVICES_JSON_PATH)
+
+# Create group of cuco devices in one group (connected lights in one room)
+cuco_devices_list = [
+    device for device in devices_list if device.device_type == "MiotDevice"]
+cuco_group_list = create_cuco_light_s14_group(cuco_devices_list)
+
+yeelight_devices_list = [
+    create_yeelight_device(device) for device in devices_list if device.device_type == "yeelight"]
+
+
+
+# Turn on and off yeelight lights and check power status
+# toggle_yeelight_device(yeelight_devices_list[0])
+# print(power_status_yeelight(yeelight_devices_list[0]))
+
+
+# Turn on and off celling cuco lights and check power status
+# on_cuco_light_s14(cuco_group_list)
+# off_cuco_light_s14(cuco_group_list)
+# print(power_status_cuco_light_s14_group(cuco_group_list))
